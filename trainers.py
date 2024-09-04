a9217ffbd755cd2ac9131c858567e29d2adfcdef
ethanherron <edherron@iastate.edu>
2024-09-03 20:24:51 -0500

    j

---
+++ trainers.py
@@ -0,0 +1,14 @@
+import jax
+import jax.numpy as jnp
+import flax
+import optax
+from chex import PRNGKey
+from typing import Any
+from functools import partial
+from torch.utils.data import DataLoader
+from models import DiT
+
+
+
+        
+    
